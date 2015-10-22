ed6cd7c4b281a070b69a8c6771f51689546e99a1
David Bernheisel <david@bernheisel.com>
2015-10-22 17:16:12 -0400

    Add display name to user model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ActiveRecord::Base
   has_many :answers
-  has_one :accepted_answer
+  has_one  :accepted_answer, ->(id) { where(accepted_answer_id: id) }, :class_name=> "Answer"
 end
