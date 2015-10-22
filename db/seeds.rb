ec0103896455c54b71372a48eef2aae5a520f6e1
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-22 17:37:35 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(full_name: "ryan", email: "r@exmple.com", password: "password", token: SecureRandom.hex, display_name: "rgraham")
+User.create!(full_name: "david", email: "d@example.com", password: "password", token: SecureRandom.hex, display_name: "dbern")
+User.create!(full_name: "terry", email: "t@example.com", password: "password", token: SecureRandom.hex, display_name: "tkalhoun")
+User.create!(full_name: "karlo", email: "k@example.com", password: "password", token: SecureRandom.hex, display_name: "keleuterio")
+
+50.times do
+  User.create!(
+    full_name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    token: SecureRandom.hex,
+    display_name: Faker::Internet.user_name)
+end
+
+users = User.all
+200.times do
+  Question.create!(
+    user_id: users.sample.id,
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph)
+end
+
+questions = Question.all
+500.times do
+  Answer.create!(
+    user_id: users.sample.id,
+    question_id: questions.sample.id,
+    answer_text: Faker::Lorem.paragraph)
+
+    
+end
