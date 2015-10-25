d187ab60410389f5fce43c6373787648fbcfad2c
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-25 12:54:03 -0400

    Fix error on test's.

--- app/controllers/meow_questions_controller.rb
+++ app/controllers/meow_questions_controller.rb
@@ -1,12 +1,12 @@
 class MeowQuestionsController < ApplicationController
   def create
-      u = User.where(token: request.headers["user-token"]).first
-      meow_question = MeowQuestion.new(meow_question_params)
-      meow_question.user_id = u.id
-      meow_question.save
+    u = User.where(token: request.headers["user-token"]).first
+    meow_question = MeowQuestion.new(meow_question_params)
+    meow_question.user_id = u.id
+    meow_question.save
   end
 
   def meow_question_params
-      params.permit(:id, :question_id, :up)
+    params.permit(:id, :question_id, :up)
   end
 end
